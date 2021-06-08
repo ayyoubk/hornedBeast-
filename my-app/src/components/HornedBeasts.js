d2c01f7fc7d286b5783839375e9ef0c454f2989c
Ayyoub Al keyyam <ayoub.alqeam@gmail.com>
2021-06-08 14:57:04 +0300

    some edit

--- my-app/src/components/HornedBeasts.js
+++ my-app/src/components/HornedBeasts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
 
 class HornedBeast extends React.Component {
   constructor(props) {
@@ -8,26 +8,27 @@ class HornedBeast extends React.Component {
       beastTitle: this.props.title,
       beastImg: this.props.img,
       beastInfo: this.props.info,
-      votesNum :0,
+      votesNum: 0,
 
     };
   }
   voteForIt = () => {
     this.setState({
-      votesNum : this.state.votesNum +=1,
+      votesNum: this.state.votesNum += 1,
     });
   }
   render() {
 
     return (
 
-      <div class='beast'>
-        <h2>{this.state.beastTitle}</h2>
-        <p>&#x2B50; {this.state.votesNum} votes</p>
-        <img src={this.state.beastImg} alt="" width='250px' height='250px' onClick={this.voteForIt} />
-        <p>{this.state.beastInfo}</p>
-      </div>
-
+      <Card border="dark" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={this.state.beastImg} onClick={this.voteForIt}/>
+        <Card.Body>
+          <Card.Title>{this.state.beastTitle}</Card.Title>
+          <Card.Text>&#x2B50; {this.state.votesNum} votes</Card.Text>
+          <Card.Text>{this.state.beastInfo}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
